a6ece89b9baa70b8b0c0bb5af720aec186712758
YanzuoLu <horn18520856657@foxmail.com>
2023-05-23 19:35:29 +0800

    speedup

--- datasets/default.py
+++ datasets/default.py
@@ -9,20 +9,19 @@ from torch.utils.data import Dataset
 
 class DefaultDataset(Dataset):
     def __init__(self, root, transform):
-        self.img_paths = []
+        self.imgs = []
         self.labels = []
         self.transform = transform
 
         with open(root) as f:
             for x in f.readlines():
                 img_path, label = x.strip().split(' ')
-                self.img_paths.append(img_path)
+                self.imgs.append(Image.open(img_path).convert('RGB'))
                 self.labels.append(int(label))
 
     def __getitem__(self, index):
-        img_path = self.img_paths[index]
         label = self.labels[index]
-        img = Image.open(img_path).convert('RGB')
+        img = self.imgs[index]
         img = self.transform(img)
         return {
             'index': index,
@@ -31,4 +30,4 @@ class DefaultDataset(Dataset):
         }
 
     def __len__(self):
-        return len(self.img_paths)
+        return len(self.imgs)
--- scripts/OVANet/Office/CDA/dslr2webcam.sh
+++ scripts/OVANet/Office/CDA/dslr2webcam.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-ulimit -n 65536
 source $(dirname "${CONDA_PYTHON_EXE}")/activate mlnet
-export OMP_NUM_THREADS=$(($(nproc --all)/$(nvidia-smi -L | wc -l)))
-export MKL_THREADING_LAYER=GNU
 export CUDA_VISIBLE_DEVICES=$1
 shift
 
