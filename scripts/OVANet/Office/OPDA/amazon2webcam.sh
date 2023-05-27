a009dc832705bf846e4d2f2d3a9637c6ed5f46fb
YanzuoLu <horn18520856657@foxmail.com>
2023-05-27 20:52:22 +0800

    uppercase

--- scripts/OVANet/Office/OPDA/amazon2webcam.sh
+++ scripts/OVANet/Office/OPDA/amazon2webcam.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source $(dirname "${CONDA_PYTHON_EXE}")/activate mlnet
+source $(dirname "${CONDA_PYTHON_EXE}")/activate MLNet
 export OMP_NUM_THREADS=$(($(nproc --all)/$(nvidia-smi -L | wc -l)))
 export CUDA_VISIBLE_DEVICES=$1
 shift
